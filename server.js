0fa67121829914b8190dd8d499490ffa1b4e7d92
Avik Saha <shaancse08@gmail.com>
2022-08-16 11:15:08 +0530

    Connection code added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const cds = require("@sap/cds");
 const { connectDatabase } = require("./srv/database/connection");
 cds.on("bootstrap", async (app) => {
-  const connection = await connectDatabase();
-  console.log(`MongoDB Connected at ${connection}`);
+  await connectDatabase();
 });
