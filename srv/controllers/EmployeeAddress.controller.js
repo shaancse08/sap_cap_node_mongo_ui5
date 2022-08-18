40da0c4c097d4308832d66e01ff01ac46fa993f1
Saha Avik (SDS/ECE-AE3) <vzh1kor@bosch.com>
2022-08-18 12:45:02 +0530

    Employee Address Method added

---
+++ srv/controllers/EmployeeAddress.controller.js
@@ -0,0 +1,24 @@
+const EmployeeAddress = require("../schemas/EmployeeAddress");
+
+const createEmployeeAddress = async (req) => {
+  // TODO Code goes here for create
+};
+
+const getEmployeeAddress = async (req) => {
+  // TODO Code goes here for get
+};
+
+const updateEmployeeAddress = async (req) => {
+  // TODO Code goes here for update
+};
+
+const deleteEmployeeAddress = async (req) => {
+  // TODO Code goes here for delete
+};
+
+module.exports = {
+  createEmployeeAddress,
+  getEmployeeAddress,
+  deleteEmployeeAddress,
+  updateEmployeeAddress,
+};
--- srv/employee-service.js
+++ srv/employee-service.js
@@ -6,6 +6,13 @@ const {
   deleteEmployeeDetails,
 } = require("./controllers/EmployeeBasicDetails.controller");
 
+const {
+  createEmployeeAddress,
+  getEmployeeAddress,
+  updateEmployeeAddress,
+  deleteEmployeeAddress,
+} = require("./controllers/EmployeeAddress.controller");
+
 const { EmployeeBasicDetails, EmployeeAddress, EmployeeSalary } = cds.entities(
   "sap.cap.mongo.EmployeeDetails"
 );
@@ -15,4 +22,9 @@ module.exports = cds.service.impl(async (srv) => {
   srv.on("READ", "EmployeeBasicDetails", getEmployeeBasicDetails);
   srv.on("UPDATE", "EmployeeBasicDetails", updateEmployeeBasicDetails);
   srv.on("DELETE", "EmployeeBasicDetails", deleteEmployeeDetails);
+
+  srv.on("CREATE", "EmployeeAddress", createEmployeeAddress);
+  srv.on("READ", "EmployeeAddress", getEmployeeAddress);
+  srv.on("UPDATE", "EmployeeAddress", updateEmployeeAddress);
+  srv.on("DELETE", "EmployeeAddress", deleteEmployeeAddress);
 });
